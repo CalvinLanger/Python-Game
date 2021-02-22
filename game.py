a50e6e2e0d85ff8bf1625501a6dc9658e6defe11
Bartlomiej Langer <57857724+CalvinLanger@users.noreply.github.com>
2021-02-22 21:24:03 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 #Made by Calvin Langer
 
-print("Welcome to simple game writing in Python Language!\n")
+print("Welcome to simple game writing in Python Language!")
 
-name = input("What is your name?\n")
-
-age = int(input("What your age?\n"))
+name = input("Write your name: ")
+age = int(input("Write your age: "))
 
 if age >= 18:
-    print("You have good age to play!\n")
+    print("You have good age to play!")
 else:
-    print("You are to young to play adventure!\n")
+    print("You are to young to play traveler!")
 
-print("Okay" + " " + name + " " + "it's time to travel!")
+print("Okay", name, "it's time to travel!")
 
