eb64e7527efb032e293ff7136fd41d89d854a78c
Bartlomiej Langer <57857724+CalvinLanger@users.noreply.github.com>
2021-03-01 12:22:16 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 #Made by Calvin Langer
 
 
+
+
 #Intro to the game
 white_rose_words = [
 "White Rose speaking...",
@@ -17,39 +19,48 @@ for x in white_rose_words:
 print("You're trapped in a dungeon with your friend")
 
 
-barrel = str(input("You see the barrel. What do you do? \n"))
+barrel = str(input("You see the barrel. What do you do? \nMove the barrel / Nothing \n"))
 
 if barrel == "Move the barrel":
     
-    secret_tunnel = str(input("The barrel rolls aside and you find a secret tunnel. What do you do? \n"))
+    secret_tunnel = str(input("The barrel rolls aside and you find a secret tunnel. What do you do? \nEnter tunnel / Nothing \n"))
     if secret_tunnel == "Enter tunnel":
         print("You start to escape but your friend is too weak to go with you")
         
-        hand_note = str(input("They hand you a note. What do you do? \n"))
+        hand_note = str(input("They hand you a note. What do you do? \nLeave / Read them / Light \n"))
         if hand_note == "Leave":
             print("You crawl through the tunnel and the tunnel leads you to a beach")
 
-            look = str(input("What do you do? \n"))
-            if look == "Look":
+            leave = str(input("What do you do? \nLook / Nothing \n"))
+            if leave == "Look":
                 print("In the water you see a boat")
 
-                boat = str(input("What do you do? \n"))
+                boat = str(input("What do you do? \nGet on the boat / Nothing \n"))
                 if boat == "Get on the boat":
                     print("Congratulations, you're heading to a new world.")
-                    print("Do you want to play again?") #Add function to start on beggining!
+                    print("Do you want to play again?")
+
+                    #Back to beggning funkction ---
+                    to_the_beggining = str(input("Yes / No \n"))
+                    if to_the_beggining == "Yes":
+                        print("Start new game!")
                 else:
-                print("You died on the beach...")
+                    print("You died on the beach...")
     
-        elif secret_tunnel == "Read them":
-            print("It is too dark to read the note. What do you do? \n") #Add function back to the hand_note statment
+        elif hand_note == "Read them":
+            print("It is too dark to read the note.")
 
-        elif secret_tunnel == "Light a match":
-            print("The note says: Don't "/"leave me here")
-            
-            stay = str(input("Dou you leave your friend or stay?"))
-            if stay == "Stay":
-                print("That's over... You prove it, your heart is big...")
+            light = str(input("What do you do? \nLight a match / Leave \n"))
+            if light == "Light a match":
+                print("The note says: Don't leave me here")   
 
+                stay = str(input("Do you leave your friend or stay? \nStay / Leave \n"))
+                if stay == "Stay":
+                    print("That's over... You prove it, your heart is big...")  
+                else:
+                    leave
+            else: 
+                leave
         else:
             print("You died...")
     else:
